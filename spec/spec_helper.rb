96e7ea686a97d03fc8ab3e8ec645c57c8f2c0910
Artem Vakhutinskiy <vahut@yandex.ru>
2023-03-09 14:10:51 +0000

    fix of commit 5749cacabf3b1cdb433deb37e03bb0a48b1666ec, commited: new empty database, logs and spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
-# test database
-ENV ["APP_ENV"] = "test"
+# load gems
+require "rspec"
+require "rack/test"
+
+# load the app
+require_relative "../app"
+
+# configure
+def app
+  Sinatra::Application
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
