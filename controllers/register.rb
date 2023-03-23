4a9e06805518b52927a907ff848be1eb59a72e31
Sonny Todd <stodd2@sheffield.ac.uk>
2023-03-23 12:02:46 +0000

    Fixed register error messaging, added more validation to email and password on register

--- controllers/register.rb
+++ controllers/register.rb
@@ -24,8 +24,14 @@ post "/register" do
 
     # now proceed to validation
     @username_error = "Please enter a value for username" if @username.empty?
+
+    @email_error = "Please enter a valid E-mail (example@example.com)" if @email.include?("@") == false || @email.include?(".") == false
     @email_error = "Please enter a value for your E-mail" if @email.empty?
+
+    @password_error = "Passwords must be at least 8 characters" if @password.size < 8
+    @password_error = "Passwords must contain at least 1 number" if !@password.match(/\d/) #if password string does not contain any digits
     @password_error = "Please enter a value for your password" if @password.empty?
+    
     if @username_error.nil? && @email_error.nil? && @password_error.nil?
       User.newUser(@username, @password, @email)
       User.login(@username, @password)
@@ -35,7 +41,8 @@ post "/register" do
       erb :dashboard
     else
       @submission_error = "Please correct the errors below"
+      erb :register
     end
   end
-  #erb :register
+  
 end
