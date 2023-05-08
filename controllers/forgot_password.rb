3f8f24c687b7836dba48d746c644289a82d00a64
Rohit Menon <rmenon1@sheffield.ac.uk>
2023-05-08 21:41:15 +0000

    Added css for password display page. Small bug occuring while resetting password

--- controllers/forgot_password.rb
+++ controllers/forgot_password.rb
@@ -9,29 +9,34 @@ get '/forgot_password' do
   
 # Route for processing the reset password form
 post '/forgot_password' do
-    email = params[:email]
-    username = params[:username]
-    
-    # Validate the inputs
-    if email.empty? || username.empty?
-      flash[:error] = "Please enter your email and username."
-      redirect '/forgot_password'
-    end
-    
-    # Find the user
-    user = User.first(email: email, username: username)
-    if !user
-      flash[:error] = "No user found with that email and username."
-      redirect '/forgot_password'
-    end
-    
-    # Generate a new password and update the user's password
-    new_password = SecureRandom.hex(8)
-    User.change_password(username, user.password, new_password)
-    
-    # Render the display password page with the new password
-    @new_password = new_password
-    erb :display_password
+  email = params[:email]
+  username = params[:username]
+  
+  # Validate the inputs
+  if email.empty? || username.empty?
+    @error = "Please enter your email and username."
+    erb :forgot_password
+  end
+  
+  # Find the user
+  user = User.first(email: email, username: username)
+  if !user
+    @error = "No user found with that email and username."
+    erb :forgot_password
+  end
+
+  if user.nil?
+    @error = "username not found"
+    redirect '/forgot_password'
+  end
+  
+  # Generate a new password and update the user's password
+  new_password = SecureRandom.hex(8)
+  User.change_password(username, user.password, new_password)
+  
+  # Render the display password page with the new password
+  @new_password = new_password
+  erb :display_password
 end
   
   
