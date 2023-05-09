9c41766014b1077540d6e73ffa15ea734e4d887e
Artem Vakhutinskiy <vahut@yandex.ru>
2023-05-09 01:10:06 +0000

    articles model and controller - deleted, courses's details added

--- controllers/home.rb
+++ controllers/home.rb
@@ -3,6 +3,5 @@ require "sinatra"
 get "/" do
     # sorts array of courses by rating from highest to lowest
     @coursesSortedByRating = Course.all.sort_by(&:rating).reverse
-    @articles = Article.all
     erb :home
 end
