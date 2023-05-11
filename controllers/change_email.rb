da7439277f3c35e425b13ca976e0854ff901b3eb
Oliver Castledine <ocastledine1@sheffield.ac.uk>
2023-05-11 16:42:38 +0000

    made dashboard a bit nicer and made change email actually work

--- controllers/change_email.rb
+++ controllers/change_email.rb
@@ -16,29 +16,30 @@ post '/change_email' do
   new_email = params['new_email']
   confirm_email = params['confirm_email']
 
-  # Check if new email is valid and available
-  if new_email.include?("@") == false || new_email.include?(".") == false
-    @error = "Please enter a valid E-mail (example@example.com)" 
-    erb :change_email
-  end
-  if User.checkExisting(username, new_email) == "pass"
-    @error = "That E-mail is already in use"
-    erb :change_email
-  end
   # Check if the old email is correct
   if User.authenticateEmail(username, old_email)
-    # Check if the new email and confirm email match
-    if new_email == confirm_email
-      # Change the user's email
-      User.change_email(username, old_email, new_email)
-      redirect '/dashboard'
-    else
-      @error = "New email and confirm email do not match"
+    # Check if new email is valid and available
+    if new_email.include?("@") == false || new_email.include?(".") == false
+      @error = "Please enter a valid E-mail (example@example.com)" 
       erb :change_email
+    else
+      if User.checkExisting(username, new_email) == "pass"
+        @error = "That E-mail is already in use"
+        erb :change_email
+      else
+        # Check if the new email and confirm email match
+        if new_email == confirm_email
+          # Change the user's email
+          User.change_email(username, old_email, new_email)
+          redirect '/dashboard'
+        else
+          @error = "New email and confirm email do not match"
+          erb :change_email
+        end
+      end
     end
   else
     @error = "Incorrect old email"
     erb :change_email
   end
-
 end
