7fdbf96ce4a77f33bb234eabe1add0705b18982a
Rohit Menon <rmenon1@sheffield.ac.uk>
2023-05-11 19:31:40 +0000

    Added course details page to trusted dashboard. Yet to filter out courses

---
+++ controllers/trusted_courses.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+
+# trusted_courses.rb
+db = SQLite3::Database.new("./db/test.sqlite3")
+# GET route for trusted courses
+get '/trusted_courses' do
+  if session[:logged_in] && (User.getRole(session[:username]) == "Trusted")
+  
+    @dashUsername = session[:username]
+    @dashEmail = session[:email]
+    @dashRole = session[:role]
+    @dashNationality = session[:nationality]
+    @dashCourses = session[:courses]
+
+    @courses = Course.all
+    
+    erb :trusted_courses
+  else
+    redirect '/login'
+  end
+end
