3c67b675947467ce51687d3176b5c1445c8bf5ca
Sonny Todd <stodd2@sheffield.ac.uk>
2023-05-11 17:47:19 +0000

    Added trusted column to admin dashboard

--- controllers/dashboard_admin.rb
+++ controllers/dashboard_admin.rb
@@ -9,21 +9,12 @@ get "/dashboard_admin" do
         @dashRole = session[:role]
         @dashNationality = session[:nationality]
         @dashCourses = session[:courses]
-        region_filter = params[:region]
-        degree_filter = params[:degree]
-        id_filter = params[:id]
-        role_filter = params[:role]
-        fname_filter = params[:forename]
-        surname_filter = params[:surname]
-        gender_filter = params[:gender]
-        course_filter = params[:course]
-        suspended_filter = params[:suspended]
 
         where_clauses = []
         where_args = []
 
         where_sql = where_clauses.empty? ? "" : "WHERE #{where_clauses.join(" AND ")}"
-        sql = "SELECT users.user_id, roles.role, users.first_name, users.surname, genders.gender, users.date_of_birth, regions.region, degrees.degree, courses.course_title, suspensions.suspended, login_details.username
+        sql = "SELECT users.user_id, roles.role, users.first_name, users.surname, genders.gender, users.date_of_birth, regions.region, degrees.degree, courses.course_title, suspensions.suspended, login_details.username, login_details.trusted_provider
             FROM users
             JOIN roles ON users.role = roles.role_id
             JOIN genders ON users.gender = genders.gender_id
