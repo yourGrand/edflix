c4108cd6b84e4d21a1bcd557a8d185a378aa2609
Rohit Menon <rmenon1@sheffield.ac.uk>
2023-05-12 10:50:28 +0000

    Added few tests for trusted dashboard and change password..few failures

--- controllers/dashboard_trusted.rb
+++ controllers/dashboard_trusted.rb
@@ -1,12 +1,15 @@
 require "sinatra"
 
-db = SQLite3::Database.new("./db/test.sqlite3")
-
 # Define a route for the trusted dashboard
 get '/dashboard_trusted' do
   # Check if the user is logged in and has the trusted_provider role
   if session[:logged_in]
     if session[:role] == 'Trusted'
+      @dashUsername = session[:username]
+      @dashEmail = session[:email]
+      @dashRole = session[:role]
+      @dashNationality = session[:nationality]
+      @dashCourses = session[:courses]
       erb :dashboard_trusted
     else
       erb :dashboard
