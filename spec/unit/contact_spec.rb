f1e843262d6e961c4ce7cc9a14ff4f004371504a
Mehrad Zeinali <mzeinali1@sheffield.ac.uk>
2023-05-12 12:33:20 +0000

    unit test final (prob)

--- spec/unit/contact_spec.rb
+++ spec/unit/contact_spec.rb
@@ -6,6 +6,7 @@ require_relative "../spec_helper"
 
 RSpec.describe 'contact us page' do
   describe 'GET /contact' do
+    
     #check if the page loads
     it 'loads the contact us page' do
       get '/contact'
@@ -14,6 +15,37 @@ RSpec.describe 'contact us page' do
       expect(last_response.body).to include('Contact us')
     end
 
+    # check if failure when firstname not entered
+    it 'fails to submit when firstname is not entered' do
+      post '/contact', { reason: 'course', firstname: '', lastname: 'Pork', email: 'test@example.com', message: 'Edflix is great!' }
+  
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('Please fill out all fields')
+    end
+  
+    # check if failure when lastname not entered
+    it 'fails to submit when lastname is not entered' do
+      post '/contact', { reason: 'course', firstname: 'John', lastname: '', email: 'test@example.com', message: 'Edflix is great!' }
+  
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('Please fill out all fields')
+    end
+  
+    # check if failure when email not entered
+    it 'fails to submit when email is not entered' do
+      post '/contact', { reason: 'course', firstname: 'John', lastname: 'Pork', email: '', message: 'Edflix is great!' }
+  
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('Please fill out all fields')
+    end
+  
+    # check if failure when message not entered
+    it 'fails to submit when message is not entered' do
+      post '/contact', { reason: 'course', firstname: 'John', lastname: 'Pork', email: 'test@example.com', message: '' }
+  
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('Please fill out all fields')
+    end
 
   end
 end
